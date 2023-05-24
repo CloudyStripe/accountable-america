72d6586fd14c8c198a469fbb5868cc3888faf354
Skyler Savard <ssavard@korterra.com>
2023-05-23 21:46:21 -0500

    established project.

---
+++ src/app/politician/page.tsx
@@ -0,0 +1,13 @@
+const Politician = () => {
+
+  return (
+    <div className="container">
+        <div>Politician</div>
+        <div>Graph</div>
+        <div>Total individual contributions</div>
+        <div>Total PAC contributions</div>
+    </div>
+  )
+}
+
+export default Politician;
