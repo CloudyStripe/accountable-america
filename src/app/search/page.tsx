048777e87cf0b0ef1f11808ec9ca12cada99a7ff
Skyler Savard <ssavard@korterra.com>
2023-05-30 21:34:25 -0500

    added search icons, and tests. just need some responsive design.

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,14 +1,37 @@
 'use client'
 import { MaterialInput } from '@/components/material-components/material-input/MaterialInput'
-import { Container } from '@mui/material'
+import { Container, IconButton, InputAdornment } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
+import DehazeIcon from '@mui/icons-material/Dehaze';
 import styles from './page.module.scss'
 
 const Search = () => {
   return (
-    <Container>
-        <div className={styles.header}>Main</div>
+    <Container className={styles.container}>
+        <div className={styles.header}>Accountable America</div>
         <div className={styles.searchContainer}>
-            <MaterialInput/>
+            <MaterialInput
+              className={styles.searchInput}
+              InputProps={{
+                startAdornment:
+                <InputAdornment position='start'>
+                  <IconButton
+                    aria-label='Search'
+                  >
+                    <SearchIcon className='submitButton'></SearchIcon>
+                  </IconButton>
+                </InputAdornment>,
+                endAdornment:
+                <InputAdornment position='end'>
+                  <IconButton
+                    aria-label='Enable advanced search'
+                  >
+                    <DehazeIcon className='submitButton'></DehazeIcon>
+                  </IconButton>
+                </InputAdornment>,
+              }}   
+              placeholder='Search for a candidate...'
+            />
         </div>
     </Container>
   )
--- src/components/material-components/_tests_/material-input.test.tsx
+++ src/components/material-components/_tests_/material-input.test.tsx
@@ -1,12 +1,19 @@
 import { MaterialInput } from "../material-input/MaterialInput";
 import { TextFieldProps } from '@mui/material/TextField'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import React from "react";
 
 describe('Material input component', () => {
     
     it("Render component without any issues", () => {
-        renderComponent({placeholder: 'test'})
+        renderComponent({})
+    })
+
+    it("Placeholder is set and visible", () => {
+        renderComponent({id: "mock-input", placeholder: "test search"})
+
+        const el = screen.getByPlaceholderText('test search')
+        expect(el).toBeTruthy()
     })
 
 })
